5ad04b889ba25b903eee698b329ab7555e8da88e
gutorov123 <119526644+gutorov123@users.noreply.github.com>
2022-12-04 22:13:26 +0300

    HomeWork2
    Домашняя работа по 2 семинару

---
+++ Homework2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
