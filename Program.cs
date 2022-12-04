a32773b7f3470f7c277231226eca599a9c12f3e1
gutorov123 <119526644+gutorov123@users.noreply.github.com>
2022-12-04 13:16:32 +0300

    первая задача по С#
    вторая домашнаяя задача по С#

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
-﻿Console.WriteLine("Введите первое число: ");
-int num_A = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Введите 3 числа:");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+int num_2 = Convert.ToInt32(Console.ReadLine());
+int num_3 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число: ");
-int num_B = Convert.ToInt32(Console.ReadLine());
+int max = num_1;
 
-if (num_A > num_B)
+if (num_2 > max)
 {
-Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
+max = num_2;
 }
-else
+
+if (num_3 > max)
 {
-Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+max = num_3;
 }
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
