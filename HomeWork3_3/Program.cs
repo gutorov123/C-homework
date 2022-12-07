95536508d9cae632ab8294f273bfb28757fdaae8
gutorov123 <119526644+gutorov123@users.noreply.github.com>
2022-12-08 00:01:33 +0300

    HW3
    HW3

---
+++ HomeWork3_3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int num = SetNumber("N");
+
+int[] arr = CubeTable[n];
+{
+    int[] arr = new int[n];
+
+    for (int i = 1; i<= num; i++)
+    {
+        arr[i - 1] = i * i * i;
+    }
+    return arr;
+}
+
+System.Console.WriteLine(string.Join(","), arr);
---
+++ Homework3_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int x1 = SetNumber("x1");
+int y1 = SetNumber("y1");
+int z1 = SetNumber("z1");
+int x2 = SetNumber("x2");
+int y2 = SetNumber("y2");
+int z2 = SetNumber("z2");
+
+double result = DistanceBetween3DPoint(x1, x2, y1, y2, z1, z2);
+
+double DistanceBetween3DPoint(int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    return Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+}
+System.Console.WriteLine($"result {result}");
