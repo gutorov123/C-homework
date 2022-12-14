07d1402128bf65b5d7576116643c63288ad0be80
gutorov123 <119526644+gutorov123@users.noreply.github.com>
2022-12-14 21:22:04 +0300

    HW8
    Домашнее задание по 8 семинару

---
+++ HW8_2/Program.cs
@@ -0,0 +1,64 @@
+﻿//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+Console.Write("Введите количество строк 1 массива: ");
+            int rowsA = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов 1 массива: ");
+    int columnsA = int.Parse(Console.ReadLine());
+    Console.Write("Введите количество строк 2 массива: ");
+          int rowsB = int.Parse(Console.ReadLine());
+    Console.Write("Введите количество столбцов 2 массива: ");
+    int columnsB = int.Parse(Console.ReadLine());
+if (columnsA != rowsB)
+{
+  Console.WriteLine("Такие матрицы умножать нельзя!");
+  return;
+}
+    int[,] A = GetArray(rowsA, columnsA, 0, 10);
+    int[,] B = GetArray(rowsB, columnsB, 0, 10);
+    PrintArray(A);
+    Console.WriteLine();
+    PrintArray(B);
+    Console.WriteLine();
+    PrintArray(GetMultiplicationMatrix(A,B));
+
+    int[,] GetArray(int m, int n, int min, int max)
+    {
+      int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        result[i, j] = new Random().Next(min, max + 1);
+      }
+      
+    }
+      return result;
+    }
+
+    void PrintArray(int[,] inArray)
+    {
+     for (int i = 0; i < inArray.GetLength(0); i++)
+     {
+       for (int j = 0; j < inArray.GetLength(1); j++)
+       {
+         Console.Write($"{inArray[i, j]} ");
+       }
+       Console.WriteLine();
+     }
+    }
+
+    int [,] GetMultiplicationMatrix(int[,] arrayA, int[,] arrayB)
+    {
+      int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+      for (int i = 0; i < arrayA.GetLength(0); i++)
+      {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+          for (int k = 0; k < arrayA.GetLength(1); k++)
+          {
+            arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+          }
+        }
+      }
+      return arrayC;
+    }
